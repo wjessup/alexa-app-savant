4db88c63961652bb0f7eac90c745346d540a01c1
Christopher Clark <Christopher.Clark@28seven.com>
2016-12-11 12:05:50 -0500

    Index Cleanup
    1.) Removed old notes and requires
    2.) Removed final amazon iot intents

---
+++ customIntents/enableFletchButton.js
@@ -0,0 +1,33 @@
+//Intent includes
+var savantLib = require('../savantLib');
+
+//Intent exports
+module.change_code = 1;
+module.exports = function(app,callback){
+
+//Intent meta information
+  var intentDictionary = {
+    'intentName' : 'enableFletchButton',
+    'intentVersion' : '1.0',
+    'intentDescription' : 'Enable state of amazon IOT button',
+    'intentEnabled' : 1
+  };
+
+//Intent Enable/Disable
+  if (intentDictionary.intentEnabled === 1){
+
+//Intent
+    app.intent('enableFletchButton', {
+        "slots":{}
+        ,"utterances":["{enablePrompt} {fletch|fletcher |} button"]
+      },function(req,res) {
+      console.log('enableFletchButton Intent: Fletchers Button is now enabled');
+      savantLib.writeState("userDefined.fletchButton",1);
+      res.say('Fletchers Button is now enabled').send();
+      return false;
+      }
+    );
+  }
+//Return intent meta info to index
+  callback(intentDictionary);
+};
--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 var alexa = require('alexa-app');
-var didYouMean = require('didYouMean');
-var dnode = require('dnode');
 var path = require('path')
 
 var config = require('./config');
-var zoneParse = require('./zoneParse');
-var savantLib = require('./savantLib');
 var amazonIoT = require('./amazon_IoT')
-
 
 // Allow this module to be reloaded by hotswap when changed
 module.change_code = 1;
@@ -60,8 +55,6 @@ require("fs").readdirSync(userIntentsPath).forEach(function(file) {
   }
 });
 
-
-
 //Dictionary of prompt phases to be used in utterances
 app.dictionary = {
 	//
@@ -84,91 +77,5 @@ app.dictionary = {
 	"services":["Plex","Roku","Tivo","Apple TV","Sonos"],
 	"rangePrompt":["High","Medium","Low"]
 };
-
-////////////////////////////////////////////////////////////////////////
-//
-// Custom Savant Intents - These will need some tweaking to make them work on your system
-//
-////////////////////////////////////////////////////////////////////////
-
-/////////////////
-// HVAC Intents
-/////////////////
-
-// Get current temperature for one zone
-
-
-// Get current cool point for one zone
-
-
-// Get current heat point for one zone
-
-
-// Get current mode for one zone
-
-
-//Set HVAC Mode for one zone
-
-// Set current heat point in one zone
-
-
-// Set current cool point in one zone
-
-
-
-/////////////////
-// Ceiling Fan Intents
-/////////////////
-
-
-/////////////////
-// User preset Intents
-/////////////////
-
-
-
-
-app.intent('disableFletchButton', {
-		"slots":{}
-		,"utterances":["{disablePrompt} {fletch|fletcher |} button"]
-	},function(req,res) {
-		console.log('disableFletchButton Intent: Fletchers Button is now disabled');
-		savantLib.writeState("userDefined.fletchButton",0);
-		res.say('Fletchers Button is now disabled').send();
-		return false;
-	}
-);
-
-
-app.intent('enableFletchButton', {
-		"slots":{}
-		,"utterances":["{enablePrompt} {fletch|fletcher |} button"]
-	},function(req,res) {
-	console.log('enableFletchButton Intent: Fletchers Button is now enabled');
-	savantLib.writeState("userDefined.fletchButton",1);
-	res.say('Fletchers Button is now enabled').send();
-	return false;
-	}
-);
-
-app.intent('queryFletchButton', {
-		"slots":{}
-		,"utterances":["is {fletchs|fletchers |} button {enabled|disabled}","what is the state of {fletchs|fletchers |} button"]
-	},function(req,res) {
-		//Check and inform user of status of IoT Button
-		savantLib.readState("userDefined.fletchButton" ,function(fletchButton) {
-			if (fletchButton==1){
-				console.log('queryFletchButton Intent: Fletchers Button is enabled');
-				res.say('Fletchers Button is enabled').send();
-			}else {
-				console.log('queryFletchButton Intent: Fletchers Button is currently disabled');
-				res.say('Fletchers Button is currently disabled').send();
-			};
-		});
-		return false;
-	}
-);
-
-
 
 module.exports = app;

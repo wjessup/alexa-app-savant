d10ff1aa2a4935d7bf2331d88c68b5de16525834
Christopher Clark <Christopher.Clark@28seven.com>
2016-12-29 13:32:00 -0500

    intent logging
    added some detail around intent request

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ AlexaAppServer.start( {
 	// Use preRequest to load user data on each request and add it to the request json.
 	// In reality, this data would come from a db or files, etc.
 	,preRequest: function(json,req,res) {
+		console.log("  ");
+		console.log("  ");
+		console.log("||||||||||||||||||||||||||| start "+JSON.stringify(json.request.intent.name)+" ||||||||||||||||||||||||||| ");
 		console.log("Intent Request: "+ JSON.stringify(json.request.intent));
+		console.log("  ");
 		//console.log("prestuff:"+JSON.stringify(json));
 	}
 	// Add a dummy attribute to the response
 	,postRequest: function(json,req,res) {
+		console.log("||||||||||||||||||||||||||| end ||||||||||||||||||||||||||| ");
+		console.log("  ");
+		console.log("  ");
 		//console.log("poststuff:"+JSON.stringify(json));
 	}
 } );

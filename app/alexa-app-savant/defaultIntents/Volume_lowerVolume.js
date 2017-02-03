917ae9b02d6615b9551229165e5f0f6334e957fa
Christopher Clark <Christopher.Clark@28seven.com>
2017-02-02 19:26:36 -0500

    Relative volume control

--- app/alexa-app-savant/defaultIntents/Volume_lowerVolume.js
+++ app/alexa-app-savant/defaultIntents/Volume_lowerVolume.js
@@ -23,8 +23,26 @@ module.exports = function(app,callback){
     	}, function(req,res) {
         matcher.zonesMatcher(req.slot('ZONE'), req.slot('ZONE_TWO'))//Parse requested zone and return cleanZones
         .then(function(cleanZones) {
-          return action.relativeVolume(cleanZones,-6)//Decrease volume by 12% in cleanZones
-          .thenResolve(cleanZones);
+          for (var key in cleanZones[0]){
+            var requestedZone = cleanZones[0][key]
+            savantLib.readState(requestedZone+'.RelativeVolumeOnly', function(RelativeVolumeOnly,stateIn) {
+              var originalZone = stateIn.split(".");//parse original zone
+              if (RelativeVolumeOnly === "0"){
+                savantLib.readState(originalZone[0]+'.CurrentVolume', function(currentVolume,stateIn) {
+                  var originalZone = stateIn.split(".");//parse original zone
+                  var newVolume = Number(currentVolume)-6;//adjust volume
+                  savantLib.serviceRequest([originalZone[0]],"volume","",[newVolume]);//set volume
+                });
+                console.log("setting two way volume in "+originalZone[0] )
+              }else{
+                for (var i = 0; i < 10; i++){
+                  savantLib.serviceRequest([originalZone[key],"VolumeDown"],"zone");
+                }
+                console.log("setting one way Volume in "+originalZone[0] )
+              }
+            });
+          }
+          return cleanZones
         })
         .then(function(cleanZones) {//Inform
           eventAnalytics.send(intentDictionary.intentName,cleanZones,"Zone","SetVolume");
--- app/alexa-app-savant/defaultIntents/Volume_raiseVolume.js
+++ app/alexa-app-savant/defaultIntents/Volume_raiseVolume.js
@@ -1,6 +1,7 @@
 const
   matcher = require('../lib/zoneMatcher'),
   action = require('../lib/actionLib'),
+  savantLib = require('../lib/savantLib'),
   eventAnalytics = require('../lib/eventAnalytics');
 
 module.change_code = 1;
@@ -20,8 +21,26 @@ module.exports = function(app,callback){
     	}, function(req,res) {
         matcher.zonesMatcher(req.slot('ZONE'), req.slot('ZONE_TWO'))//Parse requested zone and return cleanZones
         .then(function(cleanZones) {
-          return action.relativeVolume(cleanZones,6)//increase volume by 12% in cleanZones
-          .thenResolve(cleanZones);
+          for (var key in cleanZones[0]){
+            var requestedZone = cleanZones[0][key]
+            savantLib.readState(requestedZone+'.RelativeVolumeOnly', function(RelativeVolumeOnly,stateIn) {
+              var originalZone = stateIn.split(".");//parse original zone
+              if (RelativeVolumeOnly === "0"){
+                savantLib.readState(originalZone[0]+'.CurrentVolume', function(currentVolume,stateIn) {
+                  var originalZone = stateIn.split(".");//parse original zone
+                  var newVolume = Number(currentVolume)+6;//adjust volume
+                  savantLib.serviceRequest([originalZone[0]],"volume","",[newVolume]);//set volume
+                });
+                console.log("setting two way volume in "+originalZone[0] )
+              }else{
+                for (var i = 0; i < 10; i++){
+                  savantLib.serviceRequest([originalZone[key],"VolumeUp"],"zone");
+                }
+                console.log("setting one way Volume in "+originalZone[0] )
+              }
+            });
+          }
+          return cleanZones
         })
         .then(function(cleanZones) {//Inform
           eventAnalytics.send(intentDictionary.intentName,cleanZones,"Zone","SetVolume");

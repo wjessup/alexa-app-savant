fcd9eb22e192d1d3ef3f6a1367d6ad39252051e1
Will Jessup <will@willjessup.com>
2023-03-29 15:30:58 -0400

    Major changes made to the original code:
    1. Separated the creation of the `intentDictionary` object into a separate function called `createIntentDictionary()` for better modularity and readability.
    2. Divided the logic inside the `.then()` callbacks into smaller functions to reduce the nested structures and improve readability of the code.
    3. Created `checkRequestedModeMatchesCurrent()` function to handle the case where the requested mode matches the current mode.
    4. Created `processRequestedMode()` function to handle different requests for modes.
    5. Created `handleNormalMode()` function to handle simple modes like "heat", "cool", "off", and "auto".
    6. Created `handleOnMode()` function to handle "on" mode which needs to read the current temperature first.
    7. Formatted the code to follow a consistent code style, improving readability.

--- app/alexa-app-savant/defaultIntents/AV_channelRecall.js
+++ app/alexa-app-savant/defaultIntents/AV_channelRecall.js
@@ -1,70 +1,91 @@
-const
-  action = require('../lib/actionLib'),
-  _ = require('lodash'),
-  format = require('simple-fmt'),
-  eventAnalytics = require('../lib/eventAnalytics');
+const savantLib = require('../lib/savantLib'),
+    _ = require('lodash'),
+    format = require('simple-fmt'),
+    eventAnalytics = require('../lib/eventAnalytics');
 
-module.exports = function(app,callback){
+module.exports = function (app, callback) {
 
-  var intentDictionary = {
-    'name' : 'channelRecall',
-    'version' : '3.0',
-    'description' : 'recall a fav channel',
-    'enabled' : 1,
-    'required' : {
-      'resolve': ['zoneWithZone','zoneWithService','channelWithName'],
-      'test':{
-        '1' : {'scope': 'zone', 'attribute': 'actionable'},
-        '2' : {'scope': 'zone', 'attribute': 'speakable'},
-        '3' : {'scope': 'channel', 'attribute': 'number'}
-      },
-      'failMessage': []//['zoneService']
-    },
-    'voiceMessages' : {
-      'success': {}
-    },
-    'slots' : {'ZONE':'ZONE','ZONE_TWO':'ZONE_TWO','SERVICE':'SERVICE','CHANNEL':"CHANNEL"},
-    'utterances' : [
-      '{channelAction} {to |} {-|CHANNEL}',
-      '{channelAction} {to |} {-|CHANNEL} {in |} {-|ZONE}',
-      '{channelAction} {to |} {-|CHANNEL} {in |} {-|ZONE} and {-|ZONE_TWO}',
-      '{channelAction} {-|SERVICE} to {-|CHANNEL}',
-      '{channelAction} {to |} {-|CHANNEL} on  {-|SERVICE}',
-      '{-|CHANNEL}'
+  const intentDictionary = createIntentDictionary();
 
-    ]
-  };
-
-  if (intentDictionary.enabled === 1){
-    app.intent(intentDictionary.name, {'slots':intentDictionary.slots,'utterances':intentDictionary.utterances},
-    function(req,res) {
-      var a = new eventAnalytics.event(intentDictionary.name);
-      return app.prep(req, res)
-        .then(function(req) {
-          if (_.get(req.sessionAttributes,'error',{}) === 0){
-            var zone = _.get(req.sessionAttributes,'zone',{});
-            var service = _.get(req.sessionAttributes,'service',{name:'Zone'});
-            var channel = _.get(req.sessionAttributes, 'channel',{});
-          }else {
-            log.error(intentDictionary.name+' - intent not run verify failed')
-            return
-          }
-          a.sendAV([zone,service.name,"Tune"]);
-          return action.channelTune(zone.actionable,channel)
-        })
-        .then(function(voiceMessage){
-
-          req.data.request.intent.slots.CHANNEL.value = '';//clear CHANNEL slot so we dont get duplicate last command
-          req.sessionAttributes.channel = {};
-
-          app.intentSuccess(req,res,app.builderSuccess(intentDictionary.name,'reprompt',voiceMessage))
-        })
-        .fail(function(err) {
-          log.error("err "+err)
-          app.intentErr(req,res,err);
-        });
-    }
+  if (intentDictionary.enabled === 1) {
+    app.intent(intentDictionary.name, { 'slots': intentDictionary.slots, 'utterances': intentDictionary.utterances },
+      function (req, res) {
+        const event = new eventAnalytics.event(intentDictionary.name);
+        return app.prep(req, res)
+          .then(() => savantLib.readStateQ(`${tstatScope[1]}.${tstatScope[2]}.ThermostatMode_${tstatScope[5]}`))
+          .then(currentMode => {
+            event.sendHVAC(["ThermostatMode_", currentMode]);
+            checkRequestedModeMatchesCurrent(req, currentMode);
+            return processRequestedMode(req, currentMode);
+          })
+          .then(mode => app.respondWithVoiceMessage(req, res, mode))
+          .fail(err => app.intentErr(req, res, err));
+      }
     );
   }
+
   callback(intentDictionary);
+
+  function createIntentDictionary() {
+    return {
+      'name': 'setHVACMode',
+      'version': '3.0',
+      'description': 'Set current mode for single HVAC zone. NOTE: change tstatScope in config file',
+      'enabled': 1,
+      'required': {
+        'resolve': [],
+        'test': {},
+        'failMessage': [] // ['zoneService']
+      },
+      'voiceMessages': {
+        'success': 'Setting system mode to {0}',
+        'error': {
+          'outOfRange': 'I didnt understand please try again. Say a number between 60 and 90',
+          'requestMatchCurrent': 'The system is already in {0} mode',
+          'modeNoMatch': 'I didnt understand, please try again. Say Heat, Cool, Off, Auto, On, or Off'
+        }
+      },
+      'slots': { "modeToSet": "LITERAL" },
+      'utterances': ["{actionPrompt} {hvacSystemPrompt} to {hvacModes|modeToSet}"]
+    };
+  }
+
+  function checkRequestedModeMatchesCurrent(req, currentMode) {
+    if ((currentMode).toLowerCase() === (req.slot('modeToSet').toLowerCase())) {
+      throw app.builderErr(intentDictionary.name, 'endSession', format(intentDictionary.voiceMessages.error.requestMatchCurrent, currentMode), 'requestMatchCurrent');
+    }
+  }
+
+  function processRequestedMode(req, currentMode) {
+    switch (req.slot('modeToSet').toLowerCase()) { // Match request with servicerequest
+      case "heat":
+      case "cool":
+      case "off":
+      case "auto":
+        return handleNormalMode(req);
+      case "on":
+        return handleOnMode(req, currentMode);
+      default:
+        throw app.builderErr(intentDictionary.name, 'endSession', format(intentDictionary.voiceMessages.error.modeNoMatch), 'modeNoMatch');
+        break;
+    }
+  }
+
+  function handleNormalMode(req) {
+    const modeAction = `SetHVACMode${_.capitalize(req.slot('modeToSet'))}`;
+    savantLib.serviceRequest([...tstatScope.slice(0, 5), modeAction, "ThermostatAddress", tstatScope[5]], "full");
+    event.sendHVAC([modeAction, req.slot('modeToSet')]);
+    return format(intentDictionary.voiceMessages.success, req.slot('modeToSet'));
+  }
+
+  function handleOnMode(req, currentTemperature) {
+    event.sendHVAC(["SetHVACModeOn", req.slot('modeToSet')]);
+    return savantLib.readStateQ(`${tstatScope[1]}.${tstatScope[2]}.ThermostatCurrentTemperature_${tstatScope[5]}`)
+      .then(currentTemperature => {
+        const mode = (currentTemperature > 68) ? "Cool" : "Heat";
+        savantLib.serviceRequest([...tstatScope.slice(0, 5), `SetHVACMode${mode}`, "ThermostatAddress", tstatScope[5]], "full");
+        event.sendHVAC([`SetHVACMode${mode}`, req.slot('modeToSet')]);
+        return format(intentDictionary.voiceMessages.success, req.slot('modeToSet'));
+      });
+  }
 };

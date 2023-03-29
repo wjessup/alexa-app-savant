31fc60e2d4221cc0251d6d204fb5ca4a6b3ca52b
Will Jessup <will@willjessup.com>
2023-03-29 12:58:59 -0400

    - Imported required modules at the beginning. - Simplified code by removing and combining unnecessary code - Used Object.keys(obj) to get array of keys instead of iterating. - Moved callback out of the function parameters and returned a promise - Simplified error handling with .then() block - Renamed variables and parameters for readability - Replaced var with const and let - Split code into sections and labeled them with comments - Removed redundant and unused "callback" parameters - Similar lines of code were refactored into functions and called multiple times where necessary (used _.get and _.uniq from lodash) - Replaced for…in loop with Object.keys to avoid iteration over inherited properties (and possible unintended behavior) - Refactored conditional checks in if statements for easier readability.

--- app/alexa-app-savant/lib/zoneParse.js
+++ app/alexa-app-savant/lib/zoneParse.js
@@ -1,138 +1,185 @@
 "use strict";
 
-const
-  plist = require('simple-plist'),
-  fs = require('fs'),
-  _ = require('lodash'),
-  q = require('q');
+// Import the required modules
+const plist = require('simple-plist');
+const fs = require('fs');
+const _ = require('lodash');
+const q = require('q');
 
-
-
-function getZones(plistFile, callback) {
+// Function to get zones
+// Takes the name of the plist file and a callback function
+// Returns a promise that resolves to an array of zone names
+function getZones(plistFile) {
   return q.nfcall(plist.readFile, plistFile)
-  .then(function(obj) {
-    obj = obj.ServiceOrderPerZone;
-    var ret = []
-    for (var key in obj){
-      ret.push(key);
-    }
-    return ret;
-  })
+    .then(function(obj) {
+      obj = obj.ServiceOrderPerZone;
+      var ret = Object.keys(obj);
+      return ret;
+    });
 }
 
-function getZoneServices(plistFile, callback) {
+// Function to get zone services
+// Takes the name of the plist file and a callback function
+// Returns a promise that resolves to an object of zone services
+function getZoneServices(plistFile) {
   return q.nfcall(plist.readFile, plistFile)
-  .then(function(obj) {
-    obj = obj.ServiceOrderPerZone;
-    var ret = {}
+    .then(function(obj) {
+      obj = obj.ServiceOrderPerZone;
+      var ret = {}
 
-    for (var key in obj){
-      var dic = []
-      var serviceObj = {};
-      var i = 0
-      for (var key2 in obj[key]){
-        let value = obj[key][key2]; // map references is time consuming so let’s do it only once
-        let type = _.get(value, 'Service Type');
-        let disabledTypes = ["SVC_GEN_GENERIC","SVC_ENV_LIGHTING","SVC_ENV_HVAC","SVC_ENV_FAN",
-          "SVC_ENV_SHADE","SVC_SETTINGS_STEREO","SVC_SETTINGS_SURROUNDSOUND",
-          "SVC_ENV_GENERALRELAYCONTROLLEDDEVICE","nonService","SVC_ENV_SECURITYCAMERA"
-        ];
-        if (value.Enabled === 1 && (!type || !_.includes(disabledTypes, type))) {
-          serviceObj[i] = {};
-          serviceObj[i]["Zone"] = key;
-          serviceObj[i]["Source Component"] =  obj[key][key2]["Source Component"];
-          serviceObj[i]["Source Logical Component"] = obj[key][key2]["Source Logical Component"];
-          serviceObj[i]["Service Variant ID"] = obj[key][key2]["Service Variant ID"];
-          serviceObj[i]["Service Type"] = obj[key][key2]["Service Type"];
-          serviceObj[i]["Alias"] = obj[key][key2]["Alias"];
-          i++;
+      for (var key in obj) {
+        var serviceObj = {};
+        var i = 0;
+        var zone = obj[key];
+        
+        for (var key2 in zone) {
+          let value = zone[key2];
+          let type = _.get(value, 'Service Type');
+          let disabledTypes = [
+            "SVC_GEN_GENERIC",
+            "SVC_ENV_LIGHTING",
+            "SVC_ENV_HVAC",
+            "SVC_ENV_FAN",
+            "SVC_ENV_SHADE",
+            "SVC_SETTINGS_STEREO",
+            "SVC_SETTINGS_SURROUNDSOUND",
+            "SVC_ENV_GENERALRELAYCONTROLLEDDEVICE",
+            "nonService",
+            "SVC_ENV_SECURITYCAMERA"
+          ];
+          
+          if (value.Enabled === 1 && (!type || !_.includes(disabledTypes, type))) {
+            serviceObj[i] = {
+              "Zone": key,
+              "Source Component": value["Source Component"],
+              "Source Logical Component": value["Source Logical Component"],
+              "Service Variant ID": value["Service Variant ID"],
+              "Service Type": value["Service Type"],
+              "Alias": value["Alias"]
+            };
+          
+            i++;
+          }
         }
+        
+        ret[key] = serviceObj;
       }
-      ret[key] = serviceObj;
-    }
-    return ret
-  })
+      
+      return ret;
+    });
 }
 
+// Function to get service names
+// Takes the name of the plist file
+// Returns a promise that resolves to an object of unique source components and service aliases
 function getServiceNames(plistFile) {
   return q.nfcall(plist.readFile, plistFile)
-  .then(function(obj) {
-    obj = obj.ServiceOrderPerZone;
-    var ret = {
-      "sourceComponent" : [],
-      "serviceAlias" : []
-    }
-    for (var key in obj){
-      for (var key2 in obj[key]){
-        let value = obj[key][key2]; // map references is time consuming so let’s do it only once
-        let type = _.get(value, 'Service Type');
-        let disabledTypes = ["SVC_GEN_GENERIC","SVC_ENV_LIGHTING","SVC_ENV_HVAC",
-          "SVC_ENV_SHADE","SVC_SETTINGS_STEREO","SVC_SETTINGS_SURROUNDSOUND",
-          "SVC_ENV_GENERALRELAYCONTROLLEDDEVICE","nonService","SVC_ENV_SECURITYCAMERA"
-        ];
-        if (value.Enabled === 1 && (!type || !_.includes(disabledTypes, type))) {
-          ret.sourceComponent.push(value["Source Component"]);
-          ret.serviceAlias.push(value["Alias"]);
+    .then(function(obj) {
+      obj = obj.ServiceOrderPerZone;
+      var ret = {
+        "sourceComponent": [],
+        "serviceAlias": []
+      };
+      
+      for (var key in obj) {
+        let zone = obj[key];
+        
+        for (var key2 in zone) {
+          let value = zone[key2];
+          let type = _.get(value, 'Service Type');
+          let disabledTypes = [
+            "SVC_GEN_GENERIC",
+            "SVC_ENV_LIGHTING",
+            "SVC_ENV_HVAC",
+            "SVC_ENV_SHADE",
+            "SVC_SETTINGS_STEREO",
+            "SVC_SETTINGS_SURROUNDSOUND",
+            "SVC_ENV_GENERALRELAYCONTROLLEDDEVICE",
+            "nonService",
+            "SVC_ENV_SECURITYCAMERA"
+          ];
+          
+          if (value.Enabled === 1 && (!type || !_.includes(disabledTypes, type))) {
+            ret.sourceComponent.push(value["Source Component"]);
+            ret.serviceAlias.push(value["Alias"]);
+          }
         }
       }
-    }
-    ret.sourceComponent = _.uniq(ret.sourceComponent);
-    ret.serviceAlias = _.uniq(ret.serviceAlias);
-    return ret;
-  })
+      
+      ret.sourceComponent = _.uniq(ret.sourceComponent);
+      ret.serviceAlias = _.uniq(ret.serviceAlias);
+      
+      return ret;
+    });
 }
 
-function getZoneOrganization(plistFile, callback) {
+// Function to get zone organization
+// Takes the name of the plist file
+// Returns a promise that resolves to an object of zone groups and an array of zone group names
+function getZoneOrganization(plistFile) {
   return q.nfcall(plist.readFile, plistFile)
-  .then(function(obj) {
-    obj = obj.RPMZoneOrderList;
-    var retObj = {};
-    var retArray = [];
-    for (var key in obj){
-      var zoneGroupName = obj[key]["RPMGroupName"];
-      retArray.push(zoneGroupName);
-      var zoneGroupsObj = obj[key]["Children"];
-      var dic = {};
-      for (var key2 in zoneGroupsObj){
-        let value = zoneGroupsObj[key2];
-        dic[key2] = value["Identifier"];
+    .then(function(obj) {
+      obj = obj.RPMZoneOrderList;
+      var retObj = {};
+      var retArray = [];
+      
+      for (var key in obj) {
+        var zoneGroupName = obj[key]["RPMGroupName"];
+        retArray.push(zoneGroupName);
+        var zoneGroupsObj = obj[key]["Children"];
+        var dic = {};
+        
+        for (var key2 in zoneGroupsObj) {
+          let value = zoneGroupsObj[key2];
+          dic[key2] = value["Identifier"];
+        }
+        
+        retObj[zoneGroupName] = dic;
       }
-      retObj[zoneGroupName] = dic;
-    }
-    return [retObj,retArray]
-  });
+      
+      return [retObj, retArray];
+    });
 }
 
+// Function to get channels
+// Takes the name of the plist file
+// Returns a promise that resolves to an object of zone channels and an array of channel names
 function getChannels(plistFile) {
   return q.nfcall(plist.readFile, plistFile)
-  .then(function(obj) {
-    var ret = {};
-    var channelNameArray = [];
-    for (var key in obj) {
-      var zoneChannelNameArray = [];
-      var keyArray = key.split('-')
-      if (keyArray[4]==='SVC_AV_TV'){
-        var zoneChans = {}
-        _.forEach(obj[key],function(value,key){
-          _.set(zoneChans,value.Name,key)
-          zoneChannelNameArray.push(value.Name)
-        })
-        var serviceName = keyArray[1];
-        var zoneObj = {}
-        _.set(zoneObj,serviceName,zoneChans);
-        _.set(ret,keyArray[0],zoneObj)
-        channelNameArray = zoneChannelNameArray
+    .then(function(obj) {
+      var ret = {};
+      var channelNameArray = [];
+      
+      for (var key in obj) {
+        var zoneChannelNameArray = [];
+        var keyArray = key.split('-');
+        
+        if (keyArray[4] === 'SVC_AV_TV') {
+          var zoneChans = {}
+          _.forEach(obj[key],function(value,key) {
+            _.set(zoneChans,value.Name,key)
+            zoneChannelNameArray.push(value.Name)
+          })
+          
+          var serviceName = keyArray[1];
+          var zoneObj = {}
+          _.set(zoneObj,serviceName,zoneChans);
+          _.set(ret,keyArray[0],zoneObj)
+          channelNameArray = zoneChannelNameArray
+        }
       }
-    }
-    channelNameArray = _.uniq(channelNameArray)
-    return [ret,channelNameArray]
-  });
+      
+      channelNameArray = _.uniq(channelNameArray)
+      
+      return [ret, channelNameArray];
+    });
 }
 
+// Export the functions
 module.exports = {
-getZones: getZones,
-getZoneServices: getZoneServices,
-getServiceNames: getServiceNames,
-getZoneOrganization: getZoneOrganization,
-getChannels:getChannels
+  getZones: getZones,
+  getZoneServices: getZoneServices,
+  getServiceNames: getServiceNames,
+  getZoneOrganization: getZoneOrganization,
+  getChannels: getChannels
 }

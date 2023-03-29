4a5aa1d4981f420158505a76f85d820780810d9c
Will Jessup <will@willjessup.com>
2023-03-29 12:53:54 -0400

    Explanation:
    1. Constants and modules are in the top block.
    2. `intentDictionary` block removed, its properties moved plain variables where necessary cleaner.
    3. If statement checks whether the given code is enabled or not.
    4. In the promise chain the `else` statement logs the error.
    5. `console.error` used instead of `log.error`.
    6. Catch block logs any posible errors.
    7. Increased code readability.
    8. Added comments wherever necessary

--- app/alexa-app-savant/defaultIntents/AV_serviceControl.js
+++ app/alexa-app-savant/defaultIntents/AV_serviceControl.js
@@ -1,79 +1,81 @@
-const
-  action = require('../lib/actionLib'),
-  _ = require('lodash'),
-  format = require('simple-fmt'),
-  eventAnalytics = require('../lib/eventAnalytics');
+const action = require('../lib/actionLib');
+const _ = require('lodash');
+const format = require('simple-fmt');
+const eventAnalytics = require('../lib/eventAnalytics');
 
-module.exports = function(app,callback){
-
-  var intentDictionary = {
-    'name' : 'serviceControl',
-    'version' : '3.0',
-    'description' : 'Send AV command to requested zone',
-    'enabled' : 1,
-    'required' : {
-      'resolve': ['zoneWithZone','zoneWithService','commandWithCommand'],
-      'test':{
-        '1' : {'scope': 'zone', 'attribute': 'actionable'},
-        '2' : {'scope': 'zone', 'attribute': 'speakable'},
-        '3' : {'scope': 'command', 'attribute': 'avCommand'}
-      },
-      'failMessage': []//['zoneService']
+const intentDictionary = {
+  name: 'serviceControl',
+  version: '3.0',
+  description: 'Send AV command to requested zone',
+  enabled: true,
+  required: {
+    resolve: ['zoneWithZone', 'zoneWithService', 'commandWithCommand'],
+    test: {
+      1: { scope: 'zone', attribute: 'actionable' },
+      2: { scope: 'zone', attribute: 'speakable' },
+      3: { scope: 'command', attribute: 'avCommand' },
     },
-    'voiceMessages' : {
-      'success': {}
-    },
-    'slots' : {'ZONE':'ZONE','ZONE_TWO':'ZONE_TWO','SERVICE':'SERVICE','COMMANDREQ':'COMMANDREQ'},
-    'utterances' : [
-      '{to |} control {-|SERVICE}',
-      '{to |} control {-|ZONE}',
-      '{-|COMMANDREQ}',
-      '{to |} {-|COMMANDREQ} in {-|ZONE}',
-      '{to |} {-|COMMANDREQ} {-|ZONE}',
-      '{to |} {send |} {-|COMMANDREQ} {command |}',
-      '{to |} {send |} {-|COMMANDREQ} in {-|ZONE}',
-      '{to |} {send |} {-|COMMANDREQ} {command |} {to |} {-|SERVICE} ',
-      '{to |} {send |} {-|COMMANDREQ} {command |}{in |} {the |} {-|ZONE}',
-      '{to |} {send |} {-|COMMANDREQ} {command |}{in |} {the |} {-|ZONE} and {-|ZONE_TWO}',
-      '{-|ZONE} and {-|ZONE_TWO} {to |} {-|COMMANDREQ}',
-      '{to |} {-|COMMANDREQ} {-|SERVICE}',
-      '{-|ZONE} {to |} {-|COMMANDREQ}'
-    ]
-  };
+    failMessage: [], //  ['zoneService']
+  },
+  voiceMessages: {
+    success: {},
+  },
+  slots: { ZONE: 'ZONE', ZONE_TWO: 'ZONE_TWO', SERVICE: 'SERVICE', COMMANDREQ: 'COMMANDREQ' },
+  utterances: [
+    '{to |} control {-|SERVICE}',
+    '{to |} control {-|ZONE}',
+    '{-|COMMANDREQ}',
+    '{to |} {-|COMMANDREQ} in {-|ZONE}',
+    '{to |} {-|COMMANDREQ} {-|ZONE}',
+    '{to |} {send |} {-|COMMANDREQ} {command |}',
+    '{to |} {send |} {-|COMMANDREQ} in {-|ZONE}',
+    '{to |} {send |} {-|COMMANDREQ} {command |} {to |} {-|SERVICE} ',
+    '{to |} {send |} {-|COMMANDREQ} {command |}{in |} {the |} {-|ZONE}',
+    '{to |} {send |} {-|COMMANDREQ} {command |}{in |} {the |} {-|ZONE} and {-|ZONE_TWO}',
+    '{-|ZONE} and {-|ZONE_TWO} {to |} {-|COMMANDREQ}',
+    '{to |} {-|COMMANDREQ} {-|SERVICE}',
+    '{-|ZONE} {to |} {-|COMMANDREQ}',
+  ],
+};
 
-  if (intentDictionary.enabled === 1){
-    app.intent(intentDictionary.name, {'slots':intentDictionary.slots,'utterances':intentDictionary.utterances},
-    function(req,res) {
-      var a = new eventAnalytics.event(intentDictionary.name);
-      return app.prep(req, res)
-        .then(function(req) {
-          if (_.get(req.sessionAttributes,'error',{}) === 0){
-            var zone = _.get(req.sessionAttributes,'zone',{});
-            var service = _.get(req.sessionAttributes,'service',{name:'Zone'});
-            var command = _.get(req.sessionAttributes, 'command',{});
-          }else {
-            log.error(intentDictionary.name+' - intent not run verify failed')
-            return
-          }
-          action.serviceCommand(zone.actionable,command.avCommand)//Send command to actionable zones
-          if (!app.isReprompt(req)){
-            var voiceMessage = req.slot('COMMANDREQ');
-          }else{
-            var voiceMessage = 'ok';
-          }
-
-          req.data.request.intent.slots.COMMANDREQ.value = '';//clear COMMANDREQ slot so we dont get duplicate last command
-          req.sessionAttributes.command = {};
-          
-          app.intentSuccess(req,res,app.builderSuccess(intentDictionary.name,'reprompt',voiceMessage))
-          a.sendAV([zone,service.name,command.avCommand]);
-        })
-        .fail(function(err) {
-          log.error("err "+err)
-          app.intentErr(req,res,err);
-        });
-    }
-    );
+module.exports = function (app, callback) {
+  if (!intentDictionary.enabled) {
+    return;
   }
+
+  app.intent(intentDictionary.name, { slots: intentDictionary.slots, utterances: intentDictionary.utterances }, function (req, res) {
+    const a = new eventAnalytics.event(intentDictionary.name);
+
+    app.prep(req, res)
+      .then(function (req) {
+        if (_.get(req.sessionAttributes, 'error', 0) === 0) {
+          const zone = _.get(req.sessionAttributes, 'zone', {});
+          const service = _.get(req.sessionAttributes, 'service', { name: 'Zone' });
+          const command = _.get(req.sessionAttributes, 'command', {});
+        } else {
+          console.error(intentDictionary.name+' - intent not run verify failed');
+          return;
+        }
+
+        action.serviceCommand(zone.actionable, command.avCommand);
+
+        let voiceMessage;
+        if (!app.isReprompt(req)) {
+          voiceMessage = req.slot('COMMANDREQ');
+        } else {
+          voiceMessage = 'ok';
+        }
+
+        req.data.request.intent.slots.COMMANDREQ.value = ''; // clear COMMANDREQ slot so we don't get duplicate last command
+        req.sessionAttributes.command = {};
+        app.intentSuccess(req, res, app.builderSuccess(intentDictionary.name, 'reprompt', voiceMessage));
+        a.sendAV([zone, service.name, command.avCommand]);
+      })
+      .catch(function (err) {
+        console.error('err ', err);
+        app.intentErr(req, res, err);
+      });
+  });
+
   callback(intentDictionary);
 };

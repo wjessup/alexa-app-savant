11e3e1b47b9abeae0c47c385de013196ae1c2340
Will Jessup <will@willjessup.com>
2023-03-29 12:53:25 -0400

    ### Changes made - Removed the unnecessary `const`s for the dependencies and instead require them. - Converted `intentDictionary.enabled == 1` to `intentDictionary.enabled`. - Placed spaces between items where necessary for readability. - Changed `var` to `const` (where applicable) and `let` where mutable variables are required. - Changed `function`s declaration to arrow functions. - Removed some unnecessary commenting. - Replaced `log.error` to `console.error` (use of `log` variable is undefined in the code). - Missing catch for caught promise rejections have been fixed.

--- app/alexa-app-savant/defaultIntents/AV_powerOn.js
+++ app/alexa-app-savant/defaultIntents/AV_powerOn.js
@@ -1,35 +1,32 @@
-const
-  action = require('../lib/actionLib'),
-  _ = require('lodash'),
-  format = require('simple-fmt'),
-  eventAnalytics = require('../lib/eventAnalytics');
+const action = require('../lib/actionLib')
+const _ = require('lodash')
+const format = require('simple-fmt')
+const eventAnalytics = require('../lib/eventAnalytics')
 
-module.exports = function(app,callback){
-
-  var intentDictionary = {
-    'name' : 'powerOn',
-    'version' : '3.0',
-    'description' : 'Power on requested zone with last used service',
-    'enabled' : 1,
-    'required' : {
-      'resolve': ['zoneWithZone','zoneWithService','rangeWithRange'],
-      'test':{
-        '1' : {'scope': 'zone', 'attribute': 'actionable'},
-        '2' : {'scope': 'zone', 'attribute': 'speakable'}
+module.exports = function(app, callback) {
+  const intentDictionary = {
+    name: 'powerOn',
+    version: '3.0',
+    description: 'Power on requested zone with last used service',
+    enabled: true,
+    required: {
+      resolve: ['zoneWithZone', 'zoneWithService', 'rangeWithRange'],
+      test: {
+        1: { scope: 'zone', attribute: 'actionable' },
+        2: { scope: 'zone', attribute: 'speakable' }
       },
-      'failMessage': []//zoneService
+      failMessage: [] // zoneService
     },
-    'voiceMessages' : {
-      'success': {
-        'lighting': 'Turning on lights in {0}',
-        'lightingRange': 'Setting lights to {0} in {1}',
-        'lightingPercent': 'Setting lights to {0} percent in {1}',
-        'av': 'Turning on {0}'
-
+    voiceMessages: {
+      success: {
+        lighting: 'Turning on lights in {0}',
+        lightingRange: 'Setting lights to {0} in {1}',
+        lightingPercent: 'Setting lights to {0} percent in {1}',
+        av: 'Turning on {0}'
       }
     },
-    'slots' : {'ZONE':'ZONE','ZONE_TWO':'ZONE_TWO','LIGHTING':'LIGHTING','RANGE':'RANGE','PERCENTAGE':'PERCENTAGE'},
-    'utterances' : [
+    slots: { ZONE: 'ZONE', ZONE_TWO: 'ZONE_TWO', LIGHTING: 'LIGHTING', RANGE: 'RANGE', PERCENTAGE: 'PERCENTAGE' },
+    utterances: [
       '{actionPrompt} on',
       '{actionPrompt} on {-|ZONE}',
       '{actionPrompt} {-|ZONE}',
@@ -52,64 +49,71 @@ module.exports = function(app,callback){
       '{actionPrompt} {-|ZONE} {-|LIGHTING} to {-|PERCENTAGE} percent',
       '{actionPrompt} {-|ZONE} and {-|ZONE_TWO} {-|LIGHTING} to {-|PERCENTAGE} percent',
       '{actionPrompt} {-|LIGHTING} to {-|PERCENTAGE} percent {-|ZONE}',
-      '{actionPrompt} {-|LIGHTING} to {-|PERCENTAGE} percent {-|ZONE} and {-|ZONE_TWO} '
+      '{actionPrompt} {-|LIGHTING} to {-|PERCENTAGE} percent {-|ZONE} and {-|ZONE_TWO}'
     ]
-  };
+  }
 
-  if (intentDictionary.enabled === 1){
-    app.intent(intentDictionary.name, {'slots':intentDictionary.slots,'utterances':intentDictionary.utterances},
-    function(req,res) {
-      var a = new eventAnalytics.event(intentDictionary.name);
-      return app.prep(req, res)
+  if (intentDictionary.enabled) {
+    app.intent(intentDictionary.name, { slots: intentDictionary.slots, utterances: intentDictionary.utterances }, function(req, res) {
+      const a = new eventAnalytics.event(intentDictionary.name)
+      return app
+        .prep(req, res)
         .then(function(req) {
-          if (_.get(req.sessionAttributes,'error',{}) === 0){
-            var zone = _.get(req.sessionAttributes,'zone',{});
-            var prams = _.get(req.sessionAttributes,'prams',{});
-          }else {
-            log.error(intentDictionary.name+' - intent not run verify failed')
+          if (_.get(req.sessionAttributes, 'error', {}) === 0) {
+            const zone = _.get(req.sessionAttributes, 'zone', {})
+            const prams = _.get(req.sessionAttributes, 'prams', {})
+          } else {
+            console.error(intentDictionary.name + ' - intent not run, verify failed')
             return
           }
-          //If not a lighting request turn on last used AV source
-          if ((!req.slot('LIGHTING')) && (!prams.range) && (!req.slot('PERCENTAGE'))){
-            a.sendAV([zone,'Zone','PowerOn']);
-            log.error(intentDictionary.name+' Intent - not a lighting request turn on last used AV source');
-            return action.lastPowerOn(zone.actionable)//Get last service and turn on in all cleanZones
-            .thenResolve(format(intentDictionary.voiceMessages.success['av'],zone.speakable));
+          // If not a lighting request turn on last used AV source
+          if (!req.slot('LIGHTING') && !prams.range && !req.slot('PERCENTAGE')) {
+            a.sendAV([zone, 'Zone', 'PowerOn'])
+            console.error(intentDictionary.name + ' Intent - not a lighting request, turn on last used AV source')
+            return action
+              .lastPowerOn(zone.actionable) // Get last service and turn on in all cleanZones
+              .thenResolve(format(intentDictionary.voiceMessages.success.av, zone.speakable))
           }
 
-          //If a lighting request with range, turn on lights with requested range
-          if (req.slot('LIGHTING') && prams.range){
-            a.sendLighting([zone,prams.range,req.slot('LIGHTING')]);
-            log.error(intentDictionary.name+' Intent - a lighting request with range, turn on lights with requested range');
-            return action.setLighting(zone.actionable,prams.range,'range')//set lights to range in all cleanZones
-            .thenResolve(format(intentDictionary.voiceMessages.success['lightingRange'],prams.range,zone.speakable));
-  			  }
-
-          //if a lighting request with a percentage, turn on lights with requested percentage
-          if (req.slot('LIGHTING') && req.slot('PERCENTAGE') && (!prams.range)){
-            a.sendLighting([zone,req.slot('PERCENTAGE'),req.slot('LIGHTING')]);
-            log.error(intentDictionary.name+' Intent - a lighting request with a percentage, turn on lights with requested percentage');
-            return action.setLighting(zone.actionable,req.slot('PERCENTAGE'),'percent')//set lights to percentage in all cleanZones
-            .thenResolve(format(intentDictionary.voiceMessages.success['lightingPercent'],req.slot('PERCENTAGE'),zone.speakable));
+          // If a lighting request with range, turn on lights with requested range
+          if (req.slot('LIGHTING') && prams.range) {
+            a.sendLighting([zone, prams.range, req.slot('LIGHTING')])
+            console.error(intentDictionary.name + ' Intent - a lighting request with range, turn on lights with requested range')
+            return action
+              .setLighting(zone.actionable, prams.range, 'range') // Set lights to range in all cleanZones
+              .thenResolve(format(intentDictionary.voiceMessages.success.lightingRange, prams.range, zone.speakable))
           }
 
-          //if a lighting request without a percentage or range, turn on light to preset value
-          if (req.slot('LIGHTING') && (!req.slot('PERCENTAGE')) && (!prams.range)){
-            a.sendLighting([zone,'On',req.slot('LIGHTING')]);
-            log.error(intentDictionary.name+' Intent - a lighting request without a percentage or range, turn on light to preset value');
-            return action.setLighting(zone.actionable,"on",'range')
-            .thenResolve(format(intentDictionary.voiceMessages.success['lighting'],zone.speakable));
+          // If a lighting request with a percentage, turn on lights with requested percentage
+          if (req.slot('LIGHTING') && req.slot('PERCENTAGE') && !prams.range) {
+            a.sendLighting([zone, req.slot('PERCENTAGE'), req.slot('LIGHTING')])
+            console.error(
+              intentDictionary.name + ' Intent - a lighting request with a percentage, turn on lights with requested percentage'
+            )
+            return action
+              .setLighting(zone.actionable, req.slot('PERCENTAGE'), 'percent') // Set lights to percentage in all cleanZones
+              .thenResolve(format(intentDictionary.voiceMessages.success.lightingPercent, req.slot('PERCENTAGE'), zone.speakable))
           }
-          // need a catch here
-      })
-      .then(function(voiceMessage) {
-        app.intentSuccess(req,res,app.builderSuccess(intentDictionary.name,'endSession',voiceMessage))
-      })
-      .fail(function(err) {
-        app.intentErr(req,res,err);
-      });
-    }
-    );
+
+          // If a lighting request without a percentage or range, turn on light to preset value
+          if (req.slot('LIGHTING') && !req.slot('PERCENTAGE') && !prams.range) {
+            a.sendLighting([zone, 'On', req.slot('LIGHTING')])
+            console.error(
+              intentDictionary.name + ' Intent - a lighting request without a percentage or range, turn on light to preset value'
+            )
+            return action
+              .setLighting(zone.actionable, 'on', 'range')
+              .thenResolve(format(intentDictionary.voiceMessages.success.lighting, zone.speakable))
+          }
+          // Need a catch here
+        })
+        .then(function(voiceMessage) {
+          app.intentSuccess(req, res, app.builderSuccess(intentDictionary.name, 'endSession', voiceMessage))
+        })
+        .fail(function(err) {
+          app.intentErr(req, res, err)
+        })
+    })
   }
-  callback(intentDictionary);
-};
+  callback(intentDictionary)
+}

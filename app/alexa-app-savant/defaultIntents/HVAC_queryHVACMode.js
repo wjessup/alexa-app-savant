9ac3f8647d4155e518d200a9c2a537a3c2ab3cba
Will Jessup <will@willjessup.com>
2023-03-30 15:06:26 -0400

    Here is the refactored code:
    Changes made:
    - Separated const declarations so that each constant has its own `const`.
    - Updated `enabled` value to a boolean.
    - Moved `readStateQ` call to a new function named `readState` for better reusability.
    - Renamed function declaration `function(req,res)` to `queryHVACMode(req, res)`.
    - Switched to using template literals instead of concatenating strings in the `readStateQ` call.
    - References to `intentDictionary` simplified for better readability.
    - The `app.builderSuccess` function is now receiving the `message` variable which holds the formatted success message.

--- app/alexa-app-savant/defaultIntents/HVAC_queryHVACMode.js
+++ app/alexa-app-savant/defaultIntents/HVAC_queryHVACMode.js
@@ -1,43 +1,52 @@
-const
-  savantLib = require('../lib/savantLib'),
-  format = require('simple-fmt'),
-  eventAnalytics = require('../lib/eventAnalytics');
+const savantLib = require('../lib/savantLib');
+const format = require('simple-fmt');
+const eventAnalytics = require('../lib/eventAnalytics');
 
-module.exports = function(app,callback){
+const intentDictionary = {
+  name: 'queryHVACMode',
+  version: '3.0',
+  description: 'Get current mode for single HVAC zone. NOTE: change tstatScope in config file',
+  enabled: true,
+  required: {
+    resolve: [],
+    test: {},
+  },
+  voiceMessages: {
+    success: 'The system is currently set to {0}',
+  },
+  slots: {},
+  utterances: [
+    'what mode is the {hvacSystemPrompt} in',
+    'is the {hvacSystemPrompt} on',
+    'is the {hvacSystemPrompt} off',
+  ],
+};
 
-  var intentDictionary = {
-    'name' : 'queryHVACMode',
-    'version' : '3.0',
-    'description' : 'Get current mode for single HVAC zone. NOTE: change tstatScope in config file',
-    'enabled' : 1,
-    'required' : {
-      'resolve': [],
-      'test': {}
-    },
-    'voiceMessages' : {
-      'success': 'The system is currently set to {0}'
-    },
-    'slots' : {},
-    'utterances' : ['what mode is the {hvacSystemPrompt} in','is the {hvacSystemPrompt} on','is the {hvacSystemPrompt} off']
-  };
+function readState(tstatScope) {
+  return savantLib.readStateQ(`${tstatScope[1]}.${tstatScope[2]}.ThermostatMode_${tstatScope[5]}`);
+}
 
-  if (intentDictionary.enabled === 1){
-    app.intent(intentDictionary.name, {'slots':intentDictionary.slots,'utterances':intentDictionary.utterances},
-    function(req,res) {
-      var a = new eventAnalytics.event(intentDictionary.name);
-      return app.prep(req, res)
-        .then(function (){
-          return savantLib.readStateQ(tstatScope[1]+'.'+tstatScope[2]+'.ThermostatMode_'+tstatScope[5])
-        })
-        .then(function (stateValue){
-          app.intentSuccess(req,res,app.builderSuccess(intentDictionary.name,'endSession',format(intentDictionary.voiceMessages.success,stateValue)))
-          a.sendHVAC(['ThermostatMode_',stateValue]);
-        })
-        .fail(function(err) {
-          app.intentErr(req,res,err);
-        });
-    }
-    );
+function queryHVACMode(req, res) {
+  const a = new eventAnalytics.event(intentDictionary.name);
+  return app.prep(req, res)
+    .then(() => readState(tstatScope))
+    .then((stateValue) => {
+      const message = format(intentDictionary.voiceMessages.success, stateValue);
+      app.intentSuccess(req, res, app.builderSuccess(intentDictionary.name, 'endSession', message));
+      a.sendHVAC(['ThermostatMode_', stateValue]);
+    })
+    .fail((err) => {
+      app.intentErr(req, res, err);
+    });
+}
+
+module.exports = function(app, callback) {
+  if (intentDictionary.enabled) {
+    const intent = {
+      'slots': intentDictionary.slots,
+      'utterances': intentDictionary.utterances,
+    };
+    app.intent(intentDictionary.name, intent, queryHVACMode);
   }
   callback(intentDictionary);
 };

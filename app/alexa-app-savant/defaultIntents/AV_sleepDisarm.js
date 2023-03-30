c324a401afa3c2aa4644633d87f21cd09bc754c9
Will Jessup <will@willjessup.com>
2023-03-30 15:06:20 -0400

    After refactoring, the code should look like:
    In this refactored code, we have: 
    
    - Used `const` to declare all the variables that are not going to be modified.
    - Broke down the function that handles the intent into a separate function to improve readibility.
    - Used fewer nesting to make the code more manageable.
    - Added consistent indentation.
    - Renamed the handler function to properly describe its purpose. 
    - Changed the `enabled` property from `1` to `true` to improve readability. 
    - Used `console.error` instead of `log.error` because `log` is not defined in the refactored code.
    - Added error handling to the case when `verify` fails.

--- app/alexa-app-savant/defaultIntents/AV_sleepDisarm.js
+++ app/alexa-app-savant/defaultIntents/AV_sleepDisarm.js
@@ -1,56 +1,52 @@
-const
-  action = require('../lib/actionLib'),
-  _ = require('lodash'),
-  format = require('simple-fmt'),
-  eventAnalytics = require('../lib/eventAnalytics');
+const action = require('../lib/actionLib');
+const _ = require('lodash');
+const format = require('simple-fmt');
+const eventAnalytics = require('../lib/eventAnalytics');
 
-module.exports = function(app,callback){
-
-  var intentDictionary = {
-    'name' : 'sleepDisarm',
-    'version' : '3.0',
-    'description' : 'Stop a sleep timer in a zone',
-    'enabled' : 1,
-    'required' : {
-      'resolve': ['zoneWithZone'],
-      'test':{
-        '1' : {'scope': 'zone', 'attribute': 'actionable'},
-        '2' : {'scope': 'zone', 'attribute': 'speakable'}
-      },
-      'failMessage': []
+const intentDictionary = {
+  name: 'sleepDisarm',
+  version: '3.0',
+  description: 'Stop a sleep timer in a zone',
+  enabled: true,
+  required: {
+    resolve: ['zoneWithZone'],
+    test: {
+      '1': { scope: 'zone', attribute: 'actionable' },
+      '2': { scope: 'zone', attribute: 'speakable' },
     },
-    'voiceMessages' : {
-      'success': 'Disabling timer in {0}',
-      'error':{}
-    },
-    'slots' : {'ZONE':'ZONE'},
-    'utterances' : ['{Stop|disable} {sleep |} timer in {-|ZONE}']
-  };
+    failMessage: [],
+  },
+  voiceMessages: {
+    success: 'Disabling timer in {0}',
+    error: {},
+  },
+  slots: { ZONE: 'ZONE' },
+  utterances: ['{Stop|disable} {sleep |} timer in {-|ZONE}'],
+};
 
-  if (intentDictionary.enabled === 1){
-    app.intent(intentDictionary.name, {'slots':intentDictionary.slots,'utterances':intentDictionary.utterances},
-    function(req,res) {
-      var a = new eventAnalytics.event(intentDictionary.name);
-      return app.prep(req, res)
-        .then(function(req) {
-          if (_.get(req.sessionAttributes,'error',{}) === 0){
-            var zone = _.get(req.sessionAttributes,'zone',{});
-          }else {
-            log.error(intentDictionary.name+' - intent not run verify failed')
-            return
-          }
-          action.sleepTimer(zone,'','disarm')
-          a.sendSleep([zone,'dis_sleepDisarm']);
-          return format(intentDictionary.voiceMessages.success,zone.speakable)
-        })
-        .then(function(voiceMessage) {
-          app.intentSuccess(req,res,app.builderSuccess(intentDictionary.name,'endSession',voiceMessage))
-        })
-        .fail(function(err) {
-          app.intentErr(req,res,err);
-        });
-    }
-    );
+function sleepDisarmIntentHandler(req, res) {
+  const a = new eventAnalytics.event(intentDictionary.name);
+  app.prep(req, res)
+    .then(function(req) {
+      if (_.get(req.sessionAttributes, 'error', {}) === 0) {
+        const zone = _.get(req.sessionAttributes, 'zone', {});
+        action.sleepTimer(zone, '', 'disarm');
+        a.sendSleep([zone, 'dis_sleepDisarm']);
+        const voiceMessage = format(intentDictionary.voiceMessages.success, zone.speakable);
+        app.intentSuccess(req, res, app.builderSuccess(intentDictionary.name, 'endSession', voiceMessage));
+      } else {
+        console.error(intentDictionary.name + ' - intent not run verify failed');
+        app.intentErr(req, res, new Error('Failed to verify intent.'));
+      }
+    })
+    .fail(function(err) {
+      app.intentErr(req, res, err);
+    });
+}
+
+module.exports = function (app, callback) {
+  if (intentDictionary.enabled) {
+    app.intent(intentDictionary.name, { slots: intentDictionary.slots, utterances: intentDictionary.utterances }, sleepDisarmIntentHandler);
   }
   callback(intentDictionary);
 };
